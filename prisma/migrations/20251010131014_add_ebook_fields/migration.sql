892ba377c9e7cfa026cd361ebe77c68108ada490
Joshua <joshuanoah783@gmail.com>
2025-10-27 15:20:00 +0100

    Clean up remaining deleted migration files

--- prisma/migrations/20251010131014_add_ebook_fields/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."content" ADD COLUMN     "ebookAccessType" TEXT,
-ADD COLUMN     "ebookAuthor" TEXT,
-ADD COLUMN     "ebookFileFormat" TEXT,
-ADD COLUMN     "ebookLanguage" TEXT,
-ADD COLUMN     "ebookLicenseType" TEXT,
-ADD COLUMN     "ebookPages" TEXT,
-ADD COLUMN     "ebookPrice" DOUBLE PRECISION;
